efb55565f54ba0b90d5108df7af38739a285d309
belabelaisabela <100858278+belabelaisabela@users.noreply.github.com>
2023-06-18 20:11:55 -0400

    commit css produtos

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import styles from './page.module.css'
 
 export default async function Home() {
   const router = useRouter();
@@ -28,20 +29,23 @@ export default async function Home() {
   }
 
   return (
-    <main> <Link href="/cadastro" className='voltar'> CADASTRAR </Link>
-
+    <main> 
+    <div className={styles.header}><img className={styles.logo} src='https://www.gsuplementos.com.br/checkout/tema/checkout/img/carrinho-gsuplementos/logo.svg'></img></div>
+    <h1><Link href="/cadastro" className='voltar'> CADASTRAR ITEM </Link></h1>
+    <h1>PRODUTOS</h1>
+<div >
+  <div className={styles.containerfull}>
       {produtos.map(produtos => (
-        <div key={produtos.codigo}>
-          <p style={{fontWeight: 'bold', textAlign: 'center'}}>{produtos.titulo}</p>
-          <p style={{textAlign: 'center'}}>{produtos.data}</p>
+        <div key={produtos.codigo} className={styles.tudo}>
+          <img  style={{width: 100, marginLeft: 125}} src={produtos.imagem}></img>
+          <h3 style={{fontWeight: 'bold', textAlign: 'center'}}>{produtos.titulo}</h3>
           <p style={{textAlign: 'center', fontSize: 25}}>{produtos.preco}</p>
-          <p style={{textAlign: 'center'}}>{produtos.descricao}</p>
-          <p><img  style={{width: 150, marginLeft: 880}} src={produtos.imagem}></img></p>
-          <button onClick={e => e.preventDefault(remover(produtos.codigo))}>REMOVER</button>
-          <Link href={`/produto/${produtos.codigo}`}>ver mais</Link>
-          
+          <h4 style={{textAlign: 'center', outline: 'none' }}><Link href={`/produto/${produtos.codigo}`}>ver mais</Link></h4><br/>
+          <button className={styles.submitproduto} onClick={e => e.preventDefault(remover(produtos.codigo))}>REMOVER</button><br/>
         </div>
       ))}
+      </div>
+</div>
     </main>
   )
 }
--- src/app/produto/produto [codigo]/page.js
+++ src/app/produto/produto [codigo]/page.js
@@ -4,29 +4,23 @@ import { useRouter } from "next/navigation"
 
 export default async function Produto({ params }) {
     const router = useRouter();
-    const id = { id: parseInt(params.id) }
 
-    const idJson = JSON.stringify(id);
-
-    const req = await fetch("http://localhost:3003/produto", {
-        method: "POST",
+    const req = await fetch("http://localhost:3003/produto" + params.codigo, {
+        method: "GET",
         cache: "no-cache",
         headers: { 'content-type': 'application/json' },
-        body: idJson
     })
-    const produto = await req.json();
-
-
+    const res = await req.json();
+    const produto = res[0];
 
     return (
         <div>
+            <img src={produto.imagem}/>
             <p>{produto.codigo}</p>
             <p>{produto.titulo}</p>
             <p>{produto.data}</p>
             <p>{produto.preco}</p>
             <p>{produto.descricao}</p>
-            <p>{produto.imagem}</p>
-
         </div>
 
     )
